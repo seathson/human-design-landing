f7a5c16ef1c826cb842f29fd3e049c91f409c496
seathson <ol.sadomov@gmail.com>
2020-12-10 15:00:12 +0300

    fix slider

--- src/components/services/ServicesItem.js
+++ src/components/services/ServicesItem.js
@@ -8,7 +8,7 @@ function ServicesItem(props) {
         <h3 className='services__title'>{props.title}</h3>
         <h2 className='services__price'>{props.price} р</h2>
         <ul className='services__list'>
-          {props.des.map((item, index) => <li key={index}><i className='fas fa-check'></i>{item}</li>)}
+          {props.des.map((item, index) => <li key={index}><i className='fas fa-check'></i>{item.includes('Базовое') || item.includes('Расширенное') ? <strong>{item}</strong> : item}</li>)}
         </ul>
         <a target='_blank' href={props.href} rel='noopener noreferrer' className='services__button'>
             Заказать&nbsp;<span className="fa fa-caret-right"></span>
--- src/components/swiper/SwiperList.js
+++ src/components/swiper/SwiperList.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import SwiperCore, { Navigation, Pagination, A11y } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react'
 
@@ -10,18 +10,25 @@ import 'swiper/components/scrollbar/scrollbar.scss';
 SwiperCore.use([Navigation, Pagination, A11y]);
 
 function SwiperList(props) {
+  const iframe0 = useRef(null)
+  const iframe1 = useRef(null)
+  const iframe2 = useRef(null)
+  const iframe3 = useRef(null)
+
+  const refs = [iframe0, iframe1, iframe2, iframe3]
+  
   return(
     <Swiper
       spaceBetween={0}
       slidesPerView={1}
       navigation
-      loop={true}
       pagination={{ clickable: true }}
+      onSlideChange={() => refs.forEach(elem => elem.current.contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*'))}
     >
-      <SwiperSlide style={{height: 350 + 'px', marginTop: 35 + 'px'}}><iframe title='Review' className='videoIframe' width="100%" height="90%" src="https://www.youtube.com/embed/u16I1vZeTXY" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe></SwiperSlide>
-      <SwiperSlide style={{height: 350 + 'px', marginTop: 35 + 'px'}}><iframe title='Review' className='videoIframe' width="100%" height="90%" src="https://www.youtube.com/embed/gpu7XV-kV3A" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe></SwiperSlide>
-      <SwiperSlide style={{height: 350 + 'px', marginTop: 35 + 'px'}}><iframe title='Review' className='videoIframe' width="100%" height="90%" src="https://www.youtube.com/embed/ryYctNPaiRc" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe></SwiperSlide>
-      <SwiperSlide style={{height: 350 + 'px', marginTop: 35 + 'px'}}><iframe title='Review' className='videoIframe' width="100%" height="90%" src="https://www.youtube.com/embed/S0G3vBLreLg" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe></SwiperSlide>
+      <SwiperSlide style={{height: 350 + 'px', marginTop: 35 + 'px'}}><iframe ref={iframe0} title='Review' className='videoIframe' width="100%" height="90%" src="https://www.youtube.com/embed/u16I1vZeTXY?enablejsapi=1" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe></SwiperSlide>
+      <SwiperSlide style={{height: 350 + 'px', marginTop: 35 + 'px'}}><iframe ref={iframe1} title='Review' className='videoIframe' width="100%" height="90%" src="https://www.youtube.com/embed/gpu7XV-kV3A?enablejsapi=1" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe></SwiperSlide>
+      <SwiperSlide style={{height: 350 + 'px', marginTop: 35 + 'px'}}><iframe ref={iframe2} title='Review' className='videoIframe' width="100%" height="90%" src="https://www.youtube.com/embed/ryYctNPaiRc?enablejsapi=1" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe></SwiperSlide>
+      <SwiperSlide style={{height: 350 + 'px', marginTop: 35 + 'px'}}><iframe ref={iframe3} title='Review' className='videoIframe' width="100%" height="90%" src="https://www.youtube.com/embed/S0G3vBLreLg?enablejsapi=1" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe></SwiperSlide>
     </Swiper>
   )
 }
